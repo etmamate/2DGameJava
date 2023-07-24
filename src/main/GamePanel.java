52dafc79d7e024955cd97a928d69bca3fd37d2fd
Mateus Cristo <maateus.cristo132@gmail.com>
2023-07-24 16:06:07 -0300

    playerSprite

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,3 +1,4 @@
+package main;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -5,25 +6,33 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import entity.Player;
+
+
+
 public class GamePanel extends JPanel implements Runnable {
     // SCREEN SETTINGS
     final int originalTileSize = 16;// 16x16 Tile
     final int scale = 3;//
 
-    final int tileSize = originalTileSize * scale; // 48x48 tile
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow;// 576 pixels
+
     // FPS
     int FPS = 60;
+
     KeyHandler keyH = new KeyHandler(); // Keyboard Commands
     Thread gameThread;// Cloak
 
+    Player player = new Player(this,keyH);
+
     // Set Player's default Position
-    int playerX = 100;
+    /* int playerX = 100;
     int playerY = 100;
-    int playerSpeed = 4;
+    int playerSpeed = 4; */
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -99,15 +108,7 @@ public class GamePanel extends JPanel implements Runnable {
 
 
     public void update() {
-        if (keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        } else if (keyH.downPressed == true) {
-            playerY += playerSpeed;
-        } else if (keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        } else if (keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+       player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -115,8 +116,8 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.WHITE);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
+
         g2.dispose();
     }
 }
