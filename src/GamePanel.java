6fa24069fe4fb259e72192fde95ba509152cb9a7
Mateus Cristo <maateus.cristo132@gmail.com>
2023-07-24 12:04:44 -0300

    add Gameloopdelta

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -15,7 +15,8 @@ public class GamePanel extends JPanel implements Runnable {
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow;// 576 pixels
-
+    // FPS
+    int FPS = 60;
     KeyHandler keyH = new KeyHandler(); // Keyboard Commands
     Thread gameThread;// Cloak
 
@@ -37,17 +38,65 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread = new Thread(this);
         gameThread.start();
     }
-
-    @Override
+//GAMELOOP "SLEEP"WAY #1
+/*     @Override
     public void run() {
         while (gameThread != null) {
-            System.out.println("Loop is Running");
+            // Setting FPS
+            double drawInterval = 1000000000 / FPS; // 0.01666seconds
+            double nextDrawTime = System.nanoTime() + drawInterval;
             // 1- UPDATE: update information such as character position
             update();
             // 2- DRAW: draw the screen with the updated information
             repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000;
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    } */
+    //GAMELOOP "DELTA" #2
+    public void run(){
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+        
+        while(gameThread != null){
+        
+            currentTime = System.nanoTime();
+        
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            if(timer >= 1000000000){
+                System.out.println("FPS: " + drawCount);
+                drawCount =0;
+                timer = 0;
+            }
+
         }
     }
+
 
     public void update() {
         if (keyH.upPressed == true) {
